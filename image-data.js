57780d9c28fb17f2f1709bb49a885005e2f748d9
nikhil2k333-eng <nikhil2k333@gmail.com>
2026-03-18 01:53:42 +0530

    Add files via upload

---
+++ image-data.js
@@ -0,0 +1,30 @@
+const projectImages = {
+    cenora: [
+        "images/cenora/1.jpg",
+        "images/cenora/2.jpg",
+        "images/cenora/3.jpg",
+        "images/cenora/4.jpg",
+        "images/cenora/5.jpg"
+    ],
+    orave: [
+        "images/orave/1.jpg",
+        "images/orave/2.jpg",
+        "images/orave/3.jpg",
+        "images/orave/4.jpg",
+        "images/orave/5.jpg"
+    ],
+    project3: [
+        "images/project3/1.jpg",
+        "images/project3/2.jpg",
+        "images/project3/3.jpg",
+        "images/project3/4.jpg",
+        "images/project3/5.jpg"
+    ],
+    project4: [
+        "images/project4/1.jpg",
+        "images/project4/2.jpg",
+        "images/project4/3.jpg",
+        "images/project4/4.jpg",
+        "images/project4/5.jpg"
+    ]
+};
